e9d2df59a24d419fdc82d85486b6d463fd2f8414
JesusM15 <guerrajesusm72@gmail.com>
2025-06-01 15:34:17 -0700

    comit

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ import java.io.IOException;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException {
-        MazeMenu menu = new MazeMenu();
+        MazeMenu menu = new MazeMenu(); // sdhkasj
     }
 }
